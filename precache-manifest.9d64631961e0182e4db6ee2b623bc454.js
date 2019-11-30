ae65d30c43d4d1de07784fd707dda60b233d4ae6
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-11-30 13:02:53 -0800

    Updates

---
+++ precache-manifest.9d64631961e0182e4db6ee2b623bc454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f37b664cb09b5b6ec46a1694868ecd",
+    "url": "/Bug-Out-Ready-Client/index.html"
+  },
+  {
+    "revision": "52fefb89fdc0fe3c61db",
+    "url": "/Bug-Out-Ready-Client/static/css/main.1c549fdd.chunk.css"
+  },
+  {
+    "revision": "2c701c741bc11c418c0f",
+    "url": "/Bug-Out-Ready-Client/static/js/2.41eada2c.chunk.js"
+  },
+  {
+    "revision": "52fefb89fdc0fe3c61db",
+    "url": "/Bug-Out-Ready-Client/static/js/main.012ad529.chunk.js"
+  },
+  {
+    "revision": "2a204bda635e0bd107b6",
+    "url": "/Bug-Out-Ready-Client/static/js/runtime-main.94714061.js"
+  },
+  {
+    "revision": "c7a5a4c90f4870e7190113fcc12ca54a",
+    "url": "/Bug-Out-Ready-Client/static/media/bags_bag_handbag_accessory_accessories-19-512.c7a5a4c9.png"
+  }
+]);
